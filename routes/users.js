2a0a627c104461f5dc7031dc492cc0fe1dbe4a73
vivek.bhardwaj <vivek.bhardwaj@venturepact.com>
2021-11-09 15:19:35 +0530

    creating backend

---
+++ routes/users.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+const Student = require("../models/student");
+const bcrypt =require("bcryptjs");
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/student/register', async function(req, res) {
+	try{
+	  	//Updating values to the database
+      console.log(req.body);
+      let registerUser=new Student(req.body);
+      await registerUser.save();
+      res.status(201).json("Registered Successfully");
+      }catch(err){
+      res.status(400).json(`Error is : ${err}`)
+    }
+})
+
+router.post('/student/login', async function(req,res){
+  try{
+		console.log(req.body);
+		const email=req.body.email;
+		const password=req.body.password;
+    // console.log(email);
+    // console.log(password);
+		const user = await Student.findOne({email:email});
+		console.log(user);
+    console.log(password+"moving to match"+user.password);
+		const Match = await bcrypt.compare(password,user.password);
+		console.log("Match hua "+Match);
+		const token = await user.generateAuthToken(); //Generating tokens every time user login
+		res.cookie("jwt",token,{
+			expires:new Date(Date.now() + 900000),
+			httpOnly:true
+		});
+		
+		if(Match){
+			res.status(201).json(user);
+			return true;
+		}
+		else{
+			res.status(404).json("Username or password incorrect");
+			return false;
+		}
+	}catch(err){
+		res.status(400).json(`User dose not exist`);
+	}
+})
+
+module.exports = router;
